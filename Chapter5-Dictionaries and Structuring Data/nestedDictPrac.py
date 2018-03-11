4d528b7b18d3a37376e43273219b242580f49fce
banerjeesamrat <banerjeesamrat3@gmail.com>
2018-03-12 01:26:59 +0530

    nestedDictPrac

--- Chapter5-Dictionaries and Structuring Data/nestedDictPrac.py
+++ Chapter5-Dictionaries and Structuring Data/nestedDictPrac.py
@@ -15,3 +15,4 @@ print(' - Cups           ' + str(totalBrought(allGuests, 'cups')))
 print(' - Cakes          ' + str(totalBrought(allGuests, 'cakes')))
 print(' - Ham Sandwiches ' + str(totalBrought(allGuests, 'ham sandwiches')))
 print(' - Apple Pies     ' + str(totalBrought(allGuests, 'apple pies')))
+
