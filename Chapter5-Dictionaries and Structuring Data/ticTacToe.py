541b1e8065fc32bf40a880ce5ecfb58956994c61
banerjeesamrat <banerjeesamrat3@gmail.com>
2018-03-12 00:43:57 +0530

    tic tac n etc

---
+++ Chapter5-Dictionaries and Structuring Data/ticTacToe.py
@@ -0,0 +1,37 @@
+
+#Creating a dictionary for the positions of the tic tac toe board(a data structure)
+
+theBoard= {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+#Creating a function for displaying the positions of the tic tac toe board
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(theBoard)
+
+print('\nLet us try the hard-coded game now\n')
+
+theBoard1 = {'top-L': 'O', 'top-M': 'O', 'top-R': 'O', 'mid-L': 'X', 'mid-M':
+'X', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': 'X'}
+
+printBoard(theBoard1)
+
+print('\nLet us try the automated game now\n')
+
+turn = 'X'
+for i in range(9):
+    printBoard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+printBoard(theBoard)
