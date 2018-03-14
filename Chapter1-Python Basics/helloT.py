2e470e0dae31fa8499409cf336b322a723adb138
Shripad Tak <shripad121196@gmail.com>
2018-03-14 15:40:03 +0530

    Add files via upload

---
+++ Chapter1-Python Basics/helloT.py
@@ -0,0 +1,22 @@
+'''
+    First program for Learning Automation with Python
+    Date : 14/03/2018
+    Author : stak
+'''
+#Printing Hello World
+print('Hello World ! ')
+
+#Taking name as an input
+myName = input('What is your name ? : ')
+
+#Printing name
+print('It is good to meet you '+myName)
+
+#Printing length of myName using len() function
+print('The length of your name is '+str(len(myName)))
+
+#Taking age as an input
+myAge = input('What is your age ? : ')
+
+#Printing myAge + 1
+print('You will be '+str(int(myAge) + 1)+' in a year.')
