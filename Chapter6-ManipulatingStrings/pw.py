44e33987e6a01e0cc2e126602a92689151472d42
Samrat Banerjee <banerjeesamrat3@gmail.com>
2018-03-15 23:35:25 +0530

    Update pw.py

--- Chapter6-ManipulatingStrings/pw.py
+++ Chapter6-ManipulatingStrings/pw.py
@@ -10,6 +10,7 @@
 PASSWORDS={'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
              'luggage': '12345'}
+
 # Step2-Handle Command Line Arguments
 
 import sys,pyperclip
