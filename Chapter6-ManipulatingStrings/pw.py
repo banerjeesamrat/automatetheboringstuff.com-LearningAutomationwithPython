9c4efd67ca3c7006de9e95bbdfcced0b2d8d5566
Samrat Banerjee <banerjeesamrat3@gmail.com>
2018-03-15 23:33:35 +0530

    Exciting Project1

---
+++ Chapter6-ManipulatingStrings/pw.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3.5
+# First Project of An Insecure Password Locker Program
+# Author: Samrat Banerjee
+# Date: 15-03-2018
+
+# The dictionary will be the data structure that organizes your account and password data.
+
+# Step1-Program Design and Data Structures
+
+PASSWORDS={'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage': '12345'}
+# Step2-Handle Command Line Arguments
+
+import sys,pyperclip
+
+if len(sys.argv)<2:
+ print('Usage: python pw.py[account]-copy account password')
+ sys.exit()
+
+account=sys.argv[1]	# first command line arg is the account name
+
+# Step3-Copy the Right Password
+
+if account in PASSWORDS:
+ pyperclip.copy(PASSWORDS[account])
+ print('Password for '+account+' copied to clipboard')
+else:
+ print('There is no account named '+account)
+
+
