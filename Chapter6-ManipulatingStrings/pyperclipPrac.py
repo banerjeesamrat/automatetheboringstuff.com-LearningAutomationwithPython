412eb92e923693893196bafd086782dc3940bd2a
Samrat Banerjee <banerjeesamrat3@gmail.com>
2018-03-15 22:05:46 +0530

    Create pyperclipPrac.py

---
+++ Chapter6-ManipulatingStrings/pyperclipPrac.py
@@ -0,0 +1,9 @@
+
+# Fun with copy-paste using pyperclip module
+
+>>> import pyperclip
+>>> pyperclip.copy('Hello World')
+>>> pyperclip.paste()
+'Hello World'
+>>> pyperclip.paste()
+'For example, if I copied this sentence to the clipboard and then called paste(), it would look like this:'
